3c89b9243818309e5fc8443327771414885926ce
your name <fifasisa36@gmail.com>
2023-07-31 18:40:46 +0700

    update 5

--- Lab/Lab05-If-Else-Switch-Case.cpp
+++ Lab/Lab05-If-Else-Switch-Case.cpp
@@ -36,8 +36,30 @@
 #include <stdio.h>
 
 int main() {
-
-    //--| YOUR CODE HERE
+    
+    int score = 0 ;
+    printf("Enter score:\n") ;
+    scanf("%d", &score ) ;
+    
+    if(score >= 80){
+       printf("A !") ;
+    }else if(score >= 75) {    
+       printf("B+ !") ;
+    }else if(score >= 70) {
+       printf("B !") ;
+    }else if(score >= 65) {
+       printf("C+ !") ;
+    }else if(score >= 60) {
+       printf("C !") ;
+    }else if(score >= 55) {
+       printf("D+ !") ;
+    }else if(score >= 50) {
+       printf("D !") ;
+    }else if (score < 50) {
+       printf("F !") ;
+    }else {
+       printf("F !") ;
+    }
 
     return 0 ;
 }//end main function
